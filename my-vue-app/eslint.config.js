00912d53b34a76886df368c4d8bea4029c202937
Емил Стойчев <estallinone@gmail.com>
2023-12-03 22:12:41 +0200

    add tools and remove some styles from global css

---
+++ my-vue-app/eslint.config.js
@@ -0,0 +1,13 @@
+import antfu from "@antfu/eslint-config";
+export default antfu(
+  {
+    vue: true,
+    typescript: false,
+  },
+  {
+    rules: {
+      "no-console": "off",
+      "style/semi": ["error", "always"],
+    },
+  }
+);
