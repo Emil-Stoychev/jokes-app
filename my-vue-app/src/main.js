37dcc9ae95724342023dc5b0e00de5270dc243a3
Емил Стойчев <estallinone@gmail.com>
2023-12-05 23:54:45 +0200

    add pinia to project

--- my-vue-app/src/main.js
+++ my-vue-app/src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 import router from './router'
 import VueWriter from 'vue-writer'
 
 const app = createApp(App)
+const pinia = createPinia()
+
 app.use(router)
 app.use(VueWriter)
+app.use(pinia)
 app.mount('#app')
