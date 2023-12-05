1dbaeb65117f0a4c71b8c72a355471eb9774c6da
Емил Стойчев <estallinone@gmail.com>
2023-12-05 23:56:13 +0200

    render in home from global store

--- my-vue-app/src/components/Home.vue
+++ my-vue-app/src/components/Home.vue
@@ -1,34 +1,69 @@
-<script setup>
+<script>
+import useJokeStore from '../store/jokeStore'
+import useAuthStore from '../store/authStore'
+import { onMounted } from 'vue'
+
+export default {
+  setup() {
+    const jokeStore = useJokeStore()
+    const authStore = useAuthStore()
+    onMounted(() => {
+      jokeStore.getAllJokes();
+    });
+    return { jokeStore, authStore }
+  },
+  methods: {
+    deleteJoke(jokeId) {
+      this.jokeStore.deleteCurrJoke(jokeId, 'allJokes')
+    },
+    likeToggle(jokeId) {
+      this.jokeStore.likeJokeToggle(jokeId, 'home')
+    }
+  }
+}
+
 </script>
 
 <template>
   <div class="container">
 
-    <div class="box">
+    <div v-for="joke of jokeStore.allJokes" :key="joke?._id" class="box">
       <div class="author">
-        <img class="emojie" src="/images/profile.gif" />
+        <img class="emojie" :src="`/images/${joke.author?.avatar}`" />
         <div class="authorInfo">
-          <h2>Emil Stoychev</h2>
-          <h3>Rank: 2</h3>
+          <h2>{{ joke.author?.username }}</h2>
+          <h3>Rank: {{ joke.author?.rank }}</h3>
         </div>
       </div>
 
-      <div class="textCnt">
-        <p>Some text here for this box and to test it</p>
+      <div class="textCnt" :style="{backgroundColor: joke.bgColor}">
+        <p :style="{color: joke.textColor, textAlign: joke.textAlign, fontSize: joke.size + 'rem'}" >{{ joke.text }}</p>
       </div>
 
       <div class="btns">
-        <button type="button">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="white" height="28" width="28" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm130.7 57.9c-4.2-13.6 7.1-25.9 21.3-25.9H364.5c14.2 0 25.5 12.4 21.3 25.9C369 368.4 318.2 408 258.2 408s-110.8-39.6-127.5-94.1zm86.9-85.1l0 0 0 0-.2-.2c-.2-.2-.4-.5-.7-.9c-.6-.8-1.6-2-2.8-3.4c-2.5-2.8-6-6.6-10.2-10.3c-8.8-7.8-18.8-14-27.7-14s-18.9 6.2-27.7 14c-4.2 3.7-7.7 7.5-10.2 10.3c-1.2 1.4-2.2 2.6-2.8 3.4c-.3 .4-.6 .7-.7 .9l-.2 .2 0 0 0 0 0 0c-2.1 2.8-5.7 3.9-8.9 2.8s-5.5-4.1-5.5-7.6c0-17.9 6.7-35.6 16.6-48.8c9.8-13 23.9-23.2 39.4-23.2s29.6 10.2 39.4 23.2c9.9 13.2 16.6 30.9 16.6 48.8c0 3.4-2.2 6.5-5.5 7.6s-6.9 0-8.9-2.8l0 0 0 0zm160 0l0 0-.2-.2c-.2-.2-.4-.5-.7-.9c-.6-.8-1.6-2-2.8-3.4c-2.5-2.8-6-6.6-10.2-10.3c-8.8-7.8-18.8-14-27.7-14s-18.9 6.2-27.7 14c-4.2 3.7-7.7 7.5-10.2 10.3c-1.2 1.4-2.2 2.6-2.8 3.4c-.3 .4-.6 .7-.7 .9l-.2 .2 0 0 0 0 0 0c-2.1 2.8-5.7 3.9-8.9 2.8s-5.5-4.1-5.5-7.6c0-17.9 6.7-35.6 16.6-48.8c9.8-13 23.9-23.2 39.4-23.2s29.6 10.2 39.4 23.2c9.9 13.2 16.6 30.9 16.6 48.8c0 3.4-2.2 6.5-5.5 7.6s-6.9 0-8.9-2.8l0 0 0 0 0 0z"/></svg>
+        <button type="button" @click="likeToggle(joke?._id)">
+          <svg xmlns="http://www.w3.org/2000/svg"
+            :fill="joke.likes?.find(x => x == this.authStore.user?._id) ? 'yellow' : 'white'" height="28" width="28"
+            viewBox="0 0 512 512">
+            <path
+              d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm130.7 57.9c-4.2-13.6 7.1-25.9 21.3-25.9H364.5c14.2 0 25.5 12.4 21.3 25.9C369 368.4 318.2 408 258.2 408s-110.8-39.6-127.5-94.1zm86.9-85.1l0 0 0 0-.2-.2c-.2-.2-.4-.5-.7-.9c-.6-.8-1.6-2-2.8-3.4c-2.5-2.8-6-6.6-10.2-10.3c-8.8-7.8-18.8-14-27.7-14s-18.9 6.2-27.7 14c-4.2 3.7-7.7 7.5-10.2 10.3c-1.2 1.4-2.2 2.6-2.8 3.4c-.3 .4-.6 .7-.7 .9l-.2 .2 0 0 0 0 0 0c-2.1 2.8-5.7 3.9-8.9 2.8s-5.5-4.1-5.5-7.6c0-17.9 6.7-35.6 16.6-48.8c9.8-13 23.9-23.2 39.4-23.2s29.6 10.2 39.4 23.2c9.9 13.2 16.6 30.9 16.6 48.8c0 3.4-2.2 6.5-5.5 7.6s-6.9 0-8.9-2.8l0 0 0 0zm160 0l0 0-.2-.2c-.2-.2-.4-.5-.7-.9c-.6-.8-1.6-2-2.8-3.4c-2.5-2.8-6-6.6-10.2-10.3c-8.8-7.8-18.8-14-27.7-14s-18.9 6.2-27.7 14c-4.2 3.7-7.7 7.5-10.2 10.3c-1.2 1.4-2.2 2.6-2.8 3.4c-.3 .4-.6 .7-.7 .9l-.2 .2 0 0 0 0 0 0c-2.1 2.8-5.7 3.9-8.9 2.8s-5.5-4.1-5.5-7.6c0-17.9 6.7-35.6 16.6-48.8c9.8-13 23.9-23.2 39.4-23.2s29.6 10.2 39.4 23.2c9.9 13.2 16.6 30.9 16.6 48.8c0 3.4-2.2 6.5-5.5 7.6s-6.9 0-8.9-2.8l0 0 0 0 0 0z" />
+          </svg>
+          <p class="likesCount">{{ joke?.likes?.length }}</p>
+        </button>
+        <div class="btns-sub" v-show="this.authStore?.user?._id == joke?.author?._id">
+          <button type="button">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="white" height="24" width="24" viewBox="0 0 512 512">
+              <path
+                d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z" />
+            </svg>
           </button>
-          <div class="btns-sub">
-            <button type="button">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="white" height="24" width="24" viewBox="0 0 512 512"><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg>
-            </button>
-            <button type="button">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="white" height="24" width="24" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
-            </button>
-          </div>
+          <button type="button" @click="deleteJoke(joke?._id)">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="white" height="24" width="24" viewBox="0 0 448 512">
+              <path
+                d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
 
@@ -85,15 +120,14 @@
 }
 
 .textCnt {
-  display: flex;
-  flex-direction: column;
-  height: 65%;
   overflow-y: auto;
+  height: 68%;
 }
 
-.textCnt > p {
-  padding: 1rem;
+.textCnt>p {
+  padding: 2rem 1rem;
   margin: 0;
+  word-wrap: break-word;
 }
 
 div.btns {
@@ -103,35 +137,38 @@ div.btns {
   background-color: var(--bg-color-sub);
   gap: 1rem;
   padding: 1rem;
-  position: absolute;
-  width: 100%;
-  bottom: 0;
 }
 
 div.btns-sub {
   display: flex;
   justify-content: flex-end;
   gap: 1.5rem;
-  padding-right: 2rem;
-  width: 100%;
 }
 
-div.btns > button,
-div.btns-sub > button {
+div.btns>button,
+div.btns-sub>button {
   cursor: pointer;
   border: none;
   background-color: transparent;
+  position: relative;
 }
 
-div.btns > button svg.liked {
+p.likesCount {
+  position: absolute;
+  left: 3rem;
+  top: -0.25rem;
+  z-index: 3;
+}
+
+div.btns>button svg.liked {
   fill: red;
 }
 
 
 @media screen and (max-width: 450px) {
   .container {
-  padding: 1rem 0.2rem;
-}
+    padding: 1rem 0.2rem;
+  }
 }
 </style>
 
