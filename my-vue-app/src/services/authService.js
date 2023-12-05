55e52b8e74f8c741bb607db517cae082e17a0536
Емил Стойчев <estallinone@gmail.com>
2023-12-05 23:55:27 +0200

    add authService

---
+++ my-vue-app/src/services/authService.js
@@ -0,0 +1,30 @@
+let a = window.location.origin.split(`:${window.location.port}`)
+
+const URL = a[0] + ':3030/users'
+
+export const getUserByToken = (token) => {
+    return fetch(`${URL}/getUserByToken/${token}`)
+        .then(res => res.json())
+}
+
+export const login = (data) => {
+    return fetch(`${URL}/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+        .then(res => res.json())
+}
+
+export const register = (data) => {
+    return fetch(`${URL}/register`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+        .then(res => res.json())
+}
