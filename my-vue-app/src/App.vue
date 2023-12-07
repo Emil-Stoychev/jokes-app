8ec0edeffcd0846d308fc2aa631e4afb9999bd8b
Емил Стойчев <estallinone@gmail.com>
2023-12-07 22:49:49 +0200

    add scroll top option button

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -2,8 +2,24 @@
 import Navigation from "./components/core/navigation.vue";
 import MyFooter from "./components/core/myfooter.vue";
 import useAuthStore from "./store/authStore";
+import { ref } from "vue";
 
 const authStore = useAuthStore()
+let isArrowShow = ref(false)
+
+window.addEventListener('scroll', async () => {
+  const scrollY = window.scrollY || window.pageYOffset;
+
+  if (scrollY >= 700 && !isArrowShow.value) {
+    isArrowShow.value = true
+  } else if (scrollY <= 700 && isArrowShow.value) {
+    isArrowShow.value = false
+  }
+});
+
+function goUp() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
 </script>
 
 <template>
@@ -12,6 +28,8 @@ const authStore = useAuthStore()
 
   <main class="main">
     <router-view />
+
+    <img v-if="isArrowShow" @click="goUp" class="arrowUp" src="/images/arrowUp.gif" />
   </main>
 
   <MyFooter />
@@ -30,6 +48,18 @@ const authStore = useAuthStore()
   height: 100%;
   z-index: 1000;
   top: 0;
+}
+
+/* ARROW UP */
+
+.arrowUp {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  cursor: pointer;
+  width: 150px;
+  opacity: 0.7;
+  z-index: 100;
 }
 
 .main {
