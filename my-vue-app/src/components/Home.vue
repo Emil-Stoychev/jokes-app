e2cbc2d12540a6e7db4d76afa4dbb0e3bafcca82
Емил Стойчев <estallinone@gmail.com>
2023-12-12 19:08:27 +0200

    update download image

--- my-vue-app/src/components/Home.vue
+++ my-vue-app/src/components/Home.vue
@@ -177,15 +177,20 @@ export default {
         }, 1000);
       }
     },
-    downloadAsImage() {
-      const textCnt = document.querySelector('.textCnt');
+    async downloadAsImage(joke) {
+      const currentDiv = event.currentTarget.closest('.box');
+      const textCnt = currentDiv.querySelector('.textCnt');
+      const scrollY = textCnt.scrollTop;
+      textCnt.style.height = `${textCnt.scrollHeight}px`;
+      textCnt.scrollTo(0, 0);
+      const canvas = await html2canvas(textCnt);
+      textCnt.style.height = '';
+      textCnt.scrollTo(0, scrollY);
 
-      html2canvas(textCnt).then(canvas => {
-        const link = document.createElement('a');
-        link.href = canvas.toDataURL();
-        link.download = 'downloaded-image.png';
-        link.click();
-      })
+      const link = document.createElement('a');
+      link.href = canvas.toDataURL();
+      link.download = `${joke?._id}.png`;
+      link.click();
     }
   },
 
@@ -267,7 +272,11 @@ export default {
         </div>
       </div>
 
-      <span class="downloadSpan" @click="downloadAsImage"><svg fill="white" xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 512 512"><path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg></span>
+      <span class="downloadSpan" @click="downloadAsImage(joke)"><svg fill="white" xmlns="http://www.w3.org/2000/svg" height="24"
+          width="24" viewBox="0 0 512 512">
+          <path
+            d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z" />
+        </svg></span>
 
       <div class="textCnt" :style="{ backgroundColor: joke.bgColor }">
         <p
